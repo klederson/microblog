903a0a53714b615b4e89882f255ac4e6f0576949
kisphp <mariusbogdan83@gmail.com>
2016-04-27 18:10:05 +0000

    upgraded symfony 3

--- app/autoload.php
+++ app/autoload.php
@@ -1,7 +1,18 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
 
+/**
+ * @var ClassLoader $loader
+ */
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+$dotenv = new \Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+date_default_timezone_set($_SERVER['SYMFONY_TIMEZONE']);
+
+return $loader;
