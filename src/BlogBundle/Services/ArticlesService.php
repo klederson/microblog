b7104cd346ef8048740b71ace0196294db6679cf
kisphp <mariusbogdan83@gmail.com>
2016-05-06 12:16:10 +0000

    finished articles service

--- src/BlogBundle/Services/ArticlesService.php
+++ src/BlogBundle/Services/ArticlesService.php
@@ -37,13 +37,43 @@ class ArticlesService
             $articles[] = [
                 'title' => $this->filterFileName($file),
                 'registered' => $file->getMTime(),
-                'content' => '',
+                'content' => $this->getTruncatedContent($file),
             ];
         }
 
         return $articles;
     }
 
+    /**
+     * @param SplFileInfo $fileInfo
+     * @return string
+     */
+    protected function getTruncatedContent(SplFileInfo $fileInfo)
+    {
+        $content = file_get_contents($fileInfo->getRealPath());
+        $content = $this->cleanupNewLines($content);
+        $contentLines = explode("\n", $content);
+        $firstLines = array_splice($contentLines, 0, 4);
+
+        $truncatedContent = implode("\n", $firstLines);
+
+        $cleanedContent = strip_tags($this->markdown->parse($truncatedContent));
+
+        return nl2br($cleanedContent);
+    }
+
+    /**
+     * @param $content
+     * @return mixed
+     */
+    protected function cleanupNewLines($content)
+    {
+        return preg_replace("/([\n]{2,})/", "\n", $content);
+    }
+
+    /**
+     * @return string
+     */
     protected function getMdFilesLocation()
     {
         return __DIR__ . '/../Resources/data/';
@@ -59,16 +89,19 @@ class ArticlesService
         $text = $fileInfo->getFilename();
         $text = str_replace('.md', '', $text);
 
-
         return $text;
     }
 
+    /**
+     * @param $seoTitle
+     * @return array
+     */
     public function getArticlesBySeoTitle($seoTitle)
     {
         $file = $this->getMdFilesLocation() . $seoTitle . '.md';
 
         if (!is_file($file)) {
-            throw new FileNotFoundException();
+            throw new FileNotFoundException($file);
         }
 
         $fileInfo = new \SplFileObject($file, 'r');
